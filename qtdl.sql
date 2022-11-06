2974e47634c5726824f811d40fada38c8b635201
CUONG <cuongb1910498@gmail.com>
2022-11-06 20:16:36 +0700

    file CSDL mới

--- qtdl.sql
+++ qtdl.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th10 04, 2022 lúc 08:13 AM
+-- Thời gian đã tạo: Th10 06, 2022 lúc 02:14 PM
 -- Phiên bản máy phục vụ: 10.4.25-MariaDB
 -- Phiên bản PHP: 8.1.10
 
@@ -52,59 +52,31 @@ CREATE TABLE `loai_tb` (
 --
 
 INSERT INTO `loai_tb` (`id_Loai`, `tenloai_tb`) VALUES
-('BN', 'BÀN HỌC'),
-('BNGV', 'BÀNG GV'),
+('BN', 'BÀN'),
 ('DN', 'ĐÈN'),
+('QT', 'QUẠT'),
+('TC', 'MÁY CHIẾU'),
+('TV', 'TIVI'),
 ('ĐH', 'ĐIỀU HÒA');
 
 --
 -- Bẫy `loai_tb`
 --
 DELIMITER $$
-CREATE TRIGGER `Log_DEL_loaitb` AFTER DELETE ON `loai_tb` FOR EACH ROW INSERT INTO logloai_tb(id_Loai, ten, thaotac, thoigian)
-VALUES (old.id_Loai, old.tenloai_tb, 'XÓA LOẠI THIẾT BỊ', now())
+CREATE TRIGGER `Log_capnhat_id_loai` AFTER UPDATE ON `loai_tb` FOR EACH ROW INSERT INTO system_log(thaotac, ten)
+VALUES ("CẬP NHẬT", concat("CẬP NHẬT CÁC THÀNH PHẦN CỦA LOẠI '", old.id_Loai, "'"))
 $$
 DELIMITER ;
 DELIMITER $$
-CREATE TRIGGER `Log_Update_loaitb` AFTER UPDATE ON `loai_tb` FOR EACH ROW INSERT INTO logloai_tb(id_Loai, ten, thaotac, thoigian)
-VALUES (new.id_Loai, new.tenloai_tb, 'CẬP NHẬT LOẠI TB', now())
+CREATE TRIGGER `Log_them_loai_tb` AFTER INSERT ON `loai_tb` FOR EACH ROW INSERT INTO system_log(thaotac, ten)
+VALUES ("THÊM", concat("THÊM LOẠI TB: '",new.id_Loai,"'"))
 $$
 DELIMITER ;
 DELIMITER $$
-CREATE TRIGGER `Log_insert` AFTER INSERT ON `loai_tb` FOR EACH ROW INSERT INTO logloai_tb(id_Loai, ten, thaotac, thoigian)
-VALUES (new.id_Loai, new.tenloai_tb, 'THÊM LOẠI TB', now())
+CREATE TRIGGER `Log_xoa_loaitb` AFTER DELETE ON `loai_tb` FOR EACH ROW INSERT INTO system_log(thaotac, ten)
+VALUES ("Xóa", concat("XÓA LOẠI THIẾT BỊ: ", old.tenloai_tb))
 $$
 DELIMITER ;
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `logloai_tb`
---
-
-CREATE TABLE `logloai_tb` (
-  `id` int(11) NOT NULL,
-  `id_Loai` varchar(20) NOT NULL,
-  `stt_phong` varchar(30) NOT NULL,
-  `ten` varchar(20) NOT NULL,
-  `thaotac` varchar(20) NOT NULL,
-  `thoigian` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Đang đổ dữ liệu cho bảng `logloai_tb`
---
-
-INSERT INTO `logloai_tb` (`id`, `id_Loai`, `stt_phong`, `ten`, `thaotac`, `thoigian`) VALUES
-(5, '', '', 'P5', 'THÊM PHÒNG', '2022-11-04 08:32:15'),
-(6, 'BN', 'P5', 'THÊM BN VÀO P5', 'THÊM THIẾT BỊ', '2022-11-04 08:33:05'),
-(7, 'ML', '', 'MÁY LẠNH', 'THÊM LOẠI TB', '2022-11-04 08:33:31'),
-(8, 'ĐH', '', 'ĐIỀU HÒA', 'CẬP NHẬT LOẠI TB', '2022-11-04 08:33:57'),
-(9, '', '', 'P5', 'XÓA PHÒNG', '2022-11-04 08:34:28'),
-(10, 'BN', 'P5', 'XÓA BN KHỎI PHÒNG P5', 'XÓA THIẾT BỊ', '2022-11-04 08:34:28'),
-(11, '', '', 'P4', 'XÓA PHÒNG', '2022-11-04 08:34:51'),
-(12, 'BN', 'P4', 'XÓA BN KHỎI PHÒNG P4', 'XÓA THIẾT BỊ', '2022-11-04 08:34:51'),
-(13, 'MT', 'P4', 'XÓA MT KHỎI PHÒNG P4', 'XÓA THIẾT BỊ', '2022-11-04 08:34:51');
 
 -- --------------------------------------------------------
 
@@ -124,28 +96,57 @@ CREATE TABLE `phong` (
 --
 
 INSERT INTO `phong` (`id_phong`, `stt_phong`, `cho`, `tinh_trang`) VALUES
-(1, 'P1', 55, 1),
-(2, 'P2', 100, 1),
-(3, 'P3', 45, 1);
+(16, 'P1', 50, 1),
+(17, 'P2', 45, 0),
+(21, 'P3', 12, 1);
 
 --
 -- Bẫy `phong`
 --
 DELIMITER $$
-CREATE TRIGGER `Log_suaphong` AFTER UPDATE ON `phong` FOR EACH ROW INSERt into logloai_tb(ten, thaotac, thoigian)
-VALUES (new.stt_phong , "SỬA PHÒNG" , now())
+CREATE TRIGGER `Log_sua_phong` AFTER UPDATE ON `phong` FOR EACH ROW INSERt into system_log(thaotac, ten)
+VALUES ("CẬP NHẬT", 
+        concat("CẬP NHẬT PHÒNG: '", new.stt_phong, "'"))
 $$
 DELIMITER ;
 DELIMITER $$
-CREATE TRIGGER `Log_themPhong` AFTER INSERT ON `phong` FOR EACH ROW INSERt into logloai_tb(ten, thaotac, thoigian)
-VALUES (new.stt_phong , "THÊM PHÒNG" , now())
+CREATE TRIGGER `Log_themPhong` AFTER INSERT ON `phong` FOR EACH ROW INSERt into system_log( thaotac, ten)
+VALUES ("THÊM", concat("THÊM PHÒNG: '", new.stt_phong, "'"))
 $$
 DELIMITER ;
 DELIMITER $$
-CREATE TRIGGER `Log_xoaphong` AFTER DELETE ON `phong` FOR EACH ROW INSERt into logloai_tb(ten, thaotac, thoigian)
-VALUES (old.stt_phong , "XÓA PHÒNG" , now())
+CREATE TRIGGER `Log_xoaphong` AFTER DELETE ON `phong` FOR EACH ROW INSERt into system_log(thaotac, ten)
+VALUES ("XÓA", concat("XÓA PHÒNG: '",old.stt_phong,"'"))
 $$
 DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `system_log`
+--
+
+CREATE TABLE `system_log` (
+  `id` int(11) NOT NULL,
+  `thaotac` varchar(30) NOT NULL,
+  `ten` varchar(50) NOT NULL,
+  `thoigian` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `system_log`
+--
+
+INSERT INTO `system_log` (`id`, `thaotac`, `ten`, `thoigian`) VALUES
+(24, 'THÊM', 'THÊM PHÒNG: \'P4\'', '2022-11-06 03:46:01'),
+(25, 'CẬP NHẬT', 'CẬP NHẬT PHÒNG: \'P4\'', '2022-11-06 03:46:13'),
+(26, 'XÓA', 'XÓA PHÒNG: \'P4\'', '2022-11-06 03:46:21'),
+(27, 'THÊM', 'THÊM LOẠI TB: \'ML\'', '2022-11-06 03:47:55'),
+(28, 'CẬP NHẬT', 'CẬP NHẬT CÁC THÀNH PHẦN CỦA LOẠI \'ML\'', '2022-11-06 03:48:32'),
+(29, 'Xóa', 'XÓA LOẠI THIẾT BỊ: MÁY LẠNH 1', '2022-11-06 03:48:44'),
+(30, 'THÊM', 'THÊM THIẾT BỊ MỚI \'BÀN\' VÀO PHÒNG P1', '2022-11-06 03:49:14'),
+(31, 'CẬP NHẬT', 'CẬP NHẬT \'BÀN\' CỦA PHÒNG P1', '2022-11-06 03:49:18'),
+(32, 'XÓA', 'XÓA THIẾT BỊ: \'BÀN\' KHỎI PHÒNG P1', '2022-11-06 03:49:20');
 
 -- --------------------------------------------------------
 
@@ -167,26 +168,30 @@ CREATE TABLE `tb` (
 --
 
 INSERT INTO `tb` (`id_tb`, `stt_phong`, `id_Loai`, `ten_tb`, `sl`, `sl_hu`) VALUES
-(3, 'P2', 'BN', 'Bàn 1', 10, 0),
-(4, 'P3', 'BN', 'ban 1', 100, 0),
-(12, 'P1', 'BN', 'BÀN THƯỜNG', 100, 0);
+(16, 'P3', 'BN', '100', 2331, 0),
+(17, 'P3', 'BN', 'sá', 12, 0),
+(18, 'P3', 'QT', 'QUTAJ', 12, 0),
+(19, 'P3', 'TC', 'mc', 1, 0);
 
 --
 -- Bẫy `tb`
 --
 DELIMITER $$
-CREATE TRIGGER `Log_capnhat_tb` AFTER UPDATE ON `tb` FOR EACH ROW INSERt into logloai_tb(id_Loai, stt_phong, ten, thaotac, thoigian)
-VALUES (new.id_Loai,new.stt_phong , concat('CẬP NHẬT ',new.id_Loai,' CỦA PHÒNG ',new.stt_phong) , "THÊM THIẾT BỊ" , now())
+CREATE TRIGGER `Log_capnhat_ten_tb` AFTER UPDATE ON `tb` FOR EACH ROW INSERt into system_log(thaotac, ten)
+VALUES ("CẬP NHẬT", 
+        concat("CẬP NHẬT '", old.ten_tb ,"' CỦA PHÒNG ", new.stt_phong))
 $$
 DELIMITER ;
 DELIMITER $$
-CREATE TRIGGER `Log_themtb` AFTER INSERT ON `tb` FOR EACH ROW INSERt into logloai_tb(id_Loai, stt_phong, ten, thaotac, thoigian)
-VALUES (new.id_Loai,new.stt_phong , concat('THÊM ',new.id_Loai,' VÀO ',new.stt_phong) , "THÊM THIẾT BỊ" , now())
+CREATE TRIGGER `Log_themtb` AFTER INSERT ON `tb` FOR EACH ROW INSERt into system_log(thaotac, ten)
+VALUES ("THÊM", 
+        concat("THÊM THIẾT BỊ MỚI '",new.ten_tb, "' VÀO PHÒNG ", new.stt_phong))
 $$
 DELIMITER ;
 DELIMITER $$
-CREATE TRIGGER `Log_xoatb` AFTER DELETE ON `tb` FOR EACH ROW INSERt into logloai_tb(id_Loai, stt_phong, ten, thaotac, thoigian)
-VALUES (old.id_Loai, old.stt_phong , concat('XÓA ',old.id_Loai,' KHỎI PHÒNG ',old.stt_phong) , "XÓA THIẾT BỊ" , now())
+CREATE TRIGGER `Log_xoatb` AFTER DELETE ON `tb` FOR EACH ROW INSERt into system_log(thaotac, ten)
+VALUES ("XÓA", 
+        concat("XÓA THIẾT BỊ: '",old.ten_tb ,"' KHỎI PHÒNG ", old.stt_phong))
 $$
 DELIMITER ;
 
@@ -211,7 +216,8 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`username`, `password`, `ten`, `diachi`, `sdt`, `admin`) VALUES
 ('admin', '21232f297a57a5a743894a0e4a801fc3', 'admin', 'admin', '0123456789', 1),
-('luhungcuong100', '4297f44b13955235245b2497399d7a93', 'cuong', 'BH CT AG', '1231231231', 0);
+('luhungcuong100', '4297f44b13955235245b2497399d7a93', 'cuong', 'BH CT AG', '1231231231', 0),
+('theb1910577', '4297f44b13955235245b2497399d7a93', 'THẾ GIÁM ĐỐC', 'AN GIANG', '1232123144', 0);
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -224,16 +230,16 @@ ALTER TABLE `loai_tb`
   ADD PRIMARY KEY (`id_Loai`);
 
 --
--- Chỉ mục cho bảng `logloai_tb`
---
-ALTER TABLE `logloai_tb`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Chỉ mục cho bảng `phong`
 --
 ALTER TABLE `phong`
   ADD PRIMARY KEY (`id_phong`) USING BTREE;
+
+--
+-- Chỉ mục cho bảng `system_log`
+--
+ALTER TABLE `system_log`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Chỉ mục cho bảng `tb`
@@ -252,22 +258,22 @@ ALTER TABLE `user`
 --
 
 --
--- AUTO_INCREMENT cho bảng `logloai_tb`
---
-ALTER TABLE `logloai_tb`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
-
---
 -- AUTO_INCREMENT cho bảng `phong`
 --
 ALTER TABLE `phong`
-  MODIFY `id_phong` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_phong` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT cho bảng `system_log`
+--
+ALTER TABLE `system_log`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT cho bảng `tb`
 --
 ALTER TABLE `tb`
-  MODIFY `id_tb` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id_tb` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
